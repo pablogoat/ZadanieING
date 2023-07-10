354a0e19da53b6d3ddb64c3176080dccebf556ef
Bartek <bartek_pstragowski@o2.pl>
2023-07-10 23:39:35 +0200

    fetching to database and calculating indicators

--- src/main/java/com/example/zadanie/api/TestController.java
+++ src/main/java/com/example/zadanie/api/TestController.java
@@ -1,10 +1,12 @@
 package com.example.zadanie.api;
 
 import com.example.zadanie.dao.NewDataDAO;
+import com.example.zadanie.models.OneDayData;
 import com.example.zadanie.service.FetchingDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +20,13 @@ public class TestController {
 
     private final FetchingDataService fetchingDataService;
 
-    @GetMapping
-    public ResponseEntity<List<NewDataDAO>> hello() {
+    @GetMapping(path = "/customer/{customer}")
+    public ResponseEntity<List<OneDayData>> hello(
+            @PathVariable("customer") String customerId
+    ) {
         try {
-            return ResponseEntity.ok(fetchingDataService.fetchNewData());
+            fetchingDataService.fetchNewData();
+            return ResponseEntity.ok(fetchingDataService.getCustomerData(customerId,30));
         }
         catch (IOException e){
             e.printStackTrace();
--- src/main/java/com/example/zadanie/service/FetchingDataService.java
+++ src/main/java/com/example/zadanie/service/FetchingDataService.java
@@ -1,6 +1,8 @@
 package com.example.zadanie.service;
 
 import com.example.zadanie.dao.NewDataDAO;
+import com.example.zadanie.models.OneDayData;
+import com.example.zadanie.repository.OneDayDataRepository;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.stereotype.Service;
 
@@ -8,14 +10,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
-import java.util.Properties;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class FetchingDataService {
     private final String dataPath;
+    private final OneDayDataRepository oneDayDataRepository;
 
-    public FetchingDataService() {
+    public FetchingDataService(OneDayDataRepository oneDayDataRepository) {
+        this.oneDayDataRepository = oneDayDataRepository;
         Properties properties = new Properties();
         try {
             String path = new File("src/main/resources/config.properties").getAbsolutePath();
@@ -28,10 +34,90 @@ public class FetchingDataService {
         }
     }
 
-    public List<NewDataDAO> fetchNewData() throws IOException {
-        return new CsvToBeanBuilder<NewDataDAO>(new FileReader(dataPath))
+    public void fetchNewData() throws IOException {
+        List<NewDataDAO> newData = new CsvToBeanBuilder<NewDataDAO>(new FileReader(dataPath))
                 .withType(NewDataDAO.class)
                 .build()
                 .parse();
+
+        for(NewDataDAO data : newData){
+            var dataToSave =OneDayData.builder()
+                    .date(LocalDate.parse(data.getDate()))
+                    .customerId(data.getCustomerId())
+                    .customerName((data.getCustomerName()))
+                    .startDate(LocalDate.parse(data.getStartDate()))
+                    .customerType(data.getCustomerType())
+                    .customerIncome(new BigDecimal(data.getCustomerIncome()))
+                    .customerRiskClass(data.getCustomerRiskClass())
+                    .customerBusinessType(data.getCustomerBusinessType())
+                    .R1(calculateR1(data))
+                    .R2(calculateR2(data))
+                    .build();
+
+            oneDayDataRepository.save(dataToSave);
+        }
+
     }
+
+    private double calculateR1(NewDataDAO newData){
+        // R1 = (CUSTOMER_INCOME/10) * F1
+        double R1 = Double.parseDouble(newData.getCustomerIncome()) / 10;
+
+        String customerType = newData.getCustomerType();
+
+        if (customerType.equals("TYPE_A1")) {
+            OptionalDouble avg = getCustomerData(newData.getCustomerId(), 30).stream()
+                    .mapToDouble(o -> o.getCustomerIncome().doubleValue())
+                    .average();
+
+            if (avg.isEmpty()) {
+                R1 *= 0.25;
+            }
+            else if (
+                    avg.getAsDouble() < (0.8 * Double.parseDouble(newData.getCustomerIncome()))
+            ) {
+                R1 *= 0.3;
+            }
+            else {
+                R1 *= 0.2;
+            }
+        }
+        else if (
+                customerType.equals("TYPE_A5")
+                        &&
+                newData.getCustomerRiskClass().equals("A3")){
+            String customerBusinessTypeType = newData.getCustomerBusinessType();
+
+            switch (customerBusinessTypeType) {
+                case "BR_3" -> R1 *= 0.1;
+                default -> R1 *= 0.2;
+            }
+        }
+
+        return R1;
+    }
+
+    private double calculateR2(NewDataDAO newData){
+        // R1 = (CUSTOMER_INCOME/10) * F1
+        double R2 = Double.parseDouble(newData.getCustomerIncome()) / 100;
+
+        String customerType = newData.getCustomerType();
+
+        if (customerType.equals("TYPE_A1")) {
+            String customerBusinessTypeType = newData.getCustomerBusinessType();
+
+            switch (customerBusinessTypeType) {
+                case "BT_1", "BR_2" -> R2 *= 0.05;
+                case "BR_3" -> R2 *= 0.08;
+                default -> R2 *= 0.09;
+            }
+        }
+
+        return R2;
+    }
+
+    public List<OneDayData> getCustomerData(String customerId, int rows){
+        return oneDayDataRepository.getCustomerData(customerId, rows);
+    }
+
 }
